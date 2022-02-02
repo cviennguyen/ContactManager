ecf0c87f766244f251258aff8584cec2dc360983
vien <vien@DESKTOP-GVJEV1A>
2022-02-02 17:40:39 -0500

    Add project files.

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name")]
+        public string? Firstname { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name")]
+        public string? Lastname { get; set; }
+
+        [Required(ErrorMessage = "Please enter a phone number")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter a email")]
+        public string? Email { get; set; }
+
+        public string? Organization { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1,10, ErrorMessage = "Please select a category")]
+        public int CategoryID { get; set; }
+
+        public Category? Category { get; set; }
+
+        public String Slug => Firstname?.Replace(' ', '-').ToLower() + '-' + Lastname?.Replace(' ', '-').ToLower();
+    }
+}
---
+++ ContactManager/Models/ContactContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options) {}
+        public DbSet<Contact> Contacts { get; set;}
+        public DbSet<Category> Categoriy { get; set;}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Family" },
+                new Category { CategoryId = 2, Name = "Friend" },
+                new Category { CategoryId = 3, Name = "Work" },
+                new Category { CategoryId = 4, Name = "Other" }
+                );
+        }
+    }
+}
